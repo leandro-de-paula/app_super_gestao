e2c1249ee4a2daad2babeec4ce49cb3bf7c3474e
leandro-de-paula <leandrodepaula.ti@gmailcom>
2023-06-19 06:02:31 -0300

    Optional parameters and default values

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,12 @@ Route::get('/',[\App\Http\Controllers\PrincipalController::class,'principal']);
 Route::get('/sobrenos',[\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato',[\App\Http\Controllers\ContatoController::class, 'contato']);
+
+Route::get(
+    '/contato/{name?}/{category?}/{message?}', 
+    function(
+        string $name = 'UnKnown', 
+        string $category = 'Uninformed Categoy', 
+        string $message = 'Uninformed Message'){
+    echo 'We are here: ' . $name . ' - ' . $category  . ' - ' . $message;
+});
